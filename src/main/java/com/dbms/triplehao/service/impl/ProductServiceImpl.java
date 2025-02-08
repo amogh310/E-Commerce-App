086b671ed0b64c9993b63ac3c96308a27580c3d6
Amogh Mukundrao Devarshi <100790801+amogh310@users.noreply.github.com>
2025-02-09 00:09:53 +0530

    Update ProductServiceImpl.java

--- src/main/java/com/dbms/triplehao/service/impl/ProductServiceImpl.java
+++ src/main/java/com/dbms/triplehao/service/impl/ProductServiceImpl.java
@@ -38,7 +38,7 @@ public class ProductServiceImpl implements ProductService
     @Transactional
     @Override
     public boolean addProduct(Product product) {
-        System.out.println("just adding comment new") ;
+        System.out.println("just adding comment new one") ;
         if (product.getName() != null && !"".equals(product.getName()) && product.getInventoryAmount() != null && product.getCategory() != null && !"".equals(product.getCategory()))
         {
             try {
